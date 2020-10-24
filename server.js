740c3baf736471112284d6bf68b765561b4445a4
Miguel Penuela <miguel.penuela@innovamosporti.com>
2020-10-24 00:49:26 -0500

    Mongodb connection configuration

--- server.js
+++ server.js
@@ -3,6 +3,35 @@ const express   = require('express');
 const path      = require('path');
 const http      = require('http');
 const bodyParser= require('body-parser');
+var mongoose    = require('mongoose');
+
+///////////////////////////////////////////////////////////
+//  Conexion a la base de datos MongoDB a traves de Mongoose
+var dbURI = 'mongodb://localhost/db_mean';
+mongoose.connect(dbURI,  { useNewUrlParser: true });
+
+// Configuracion de los servicios de la conexion Mongoose
+mongoose.connection.on('connected', function(){
+    console.log('Mongoose default connection open to: '+dbURI);
+});
+
+mongoose.connection.on('error', function(err){
+    console.log('Mongoose default connection error: '+err);
+});
+
+mongoose.connection.on('disconnected', function(){
+    console.log('Mongoose default connection disconnected');
+});
+
+// Si el proceso 'Node' termina, se cierra la conexion Mongoose
+process.on('SIGINT',function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose default connection disconnected through app termination.');
+        process.exit(0);
+    });
+});
+
+///////////////////////////////////////////////////////////
 
 //  Creacion y configuracion de la aplicacion express
 const app = express();
@@ -22,6 +51,9 @@ app.use(express.static(path.join(__dirname,'dist')));
 app.get('/api',function(req,res){
     res.send('La API funciona');
 });
+
+//  Configuracion de las rutas API
+require('../frontend/server/routes/tarea')(app);
 
 app.get('*',function(req,res){
     res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
